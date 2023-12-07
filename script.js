0d63e33e9d12cc96fda460724f93aca902d7e1ba
João "Daoki" Pedro <105397373+Daokisan@users.noreply.github.com>
2023-12-07 06:19:45 -0300

    update

--- script.js
+++ script.js
@@ -2,37 +2,72 @@ const menu = document.querySelector('.menu');
 const NavMenu = document.querySelector('.nav-menu');
 
 menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
+  menu.classList.toggle('ativo');
+  NavMenu.classList.toggle('ativo');
 })
 
 
 
 $(".slider").owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 4000, //2000ms = 2s;
-    responsiveClass:true,
-    margin:10,
-    autoplayHoverPause: true,
-    responsive: {
-        1200: {
-          items:3,
-           
-        },
-        768: {
-            items:2
-          },
-    
-          576: {
-            items:1
-          },
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 4000, //2000ms = 2s;
+  responsiveClass: true,
+  margin: 10,
+  autoplayHoverPause: true,
+  responsive: {
+    1200: {
+      items: 3,
 
-          480: {
-            items:1
-          },
-      }
+    },
+    768: {
+      items: 2
+    },
 
-      
-      
-    });  
+    576: {
+      items: 1
+    },
+
+    480: {
+      items: 1
+    },
+    400: {
+      items: 1
+    },
+  }
+
+
+
+});
+
+$(".portfolio-slider").owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000, //2000ms = 2s;
+  responsiveClass: true,
+  margin: 10,
+  autoplayHoverPause: true,
+  responsive: {
+    1200: {
+      items: 2,
+
+    },
+    768: {
+      items: 2
+    },
+
+    576: {
+      items: 1
+    },
+
+    480: {
+      items: 1
+    },
+    400: {
+      items: 1
+    },
+  }
+
+
+
+});  
